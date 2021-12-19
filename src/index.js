26e1ca72de71d05e529d4994d0f2cf768f14cfe0
Josué Rivas <josuerivas2311@gmail.com>
2021-12-18 18:31:34 -0600

    semantic y conf. de apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.scss';
+// import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
