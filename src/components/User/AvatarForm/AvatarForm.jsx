e19f79eb7e9ba97c7faf6e61f2df395f5b5dfd47
Josué Rivas <josuerivas2311@gmail.com>
2022-01-06 18:23:48 -0600

    drop agregado

--- src/components/User/AvatarForm/AvatarForm.jsx
+++ src/components/User/AvatarForm/AvatarForm.jsx
@@ -1,14 +1,28 @@
 import './AvatarForm.scss';
 import { Button } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
+import { useCallback } from 'react';
 
 function AvatarForm({ setShowModal }) {
+  const onDrop = useCallback((aceptedFiles) => {
+    console.log(aceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/png, image/jpeg',
+    noKeyboard: true,
+    multiple: true,
+    onDrop,
+  });
+
   return (
     <div className="avatar-form">
-      <Button>Cargar una foto</Button>
+      <Button {...getRootProps()}>Cargar una foto</Button>
       <Button>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>
         Cancelar
       </Button>
+      <input {...getInputProps()} />
     </div>
   );
 }
