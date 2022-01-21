4af8afe86500a418a451986355e5d2d6ec77fb30
Josué Rivas <josuerivas2311@gmail.com>
2022-01-21 11:39:37 -0600

    actualizar desc y sitio web

--- src/components/User/SettingForm/SettingForm.jsx
+++ src/components/User/SettingForm/SettingForm.jsx
@@ -6,6 +6,8 @@ import { useApolloClient } from '@apollo/client';
 import useAuth from '../../../hooks/useAuth';
 import PasswordForm from '../PasswordForm';
 import EmailForm from '../EmailForm/EmailForm';
+import DescriptionForm from '../DescriptionForm/DescriptionForm';
+import WebSiteForm from '../WebSiteForm';
 
 function SettingForm({
   setShowModal,
@@ -34,6 +36,28 @@ function SettingForm({
     );
   };
 
+  const onChangeDescription = () => {
+    setTitleModal('Actualiza tu descripción');
+    setClidrenModal(
+      <DescriptionForm
+        setShowModal={setShowModal}
+        currentDesc={getUser.description}
+        refetch={refetch}
+      />
+    );
+  };
+
+  const onChangeWebSite = () => {
+    setTitleModal('Actualiza tu sitio web');
+    setClidrenModal(
+      <WebSiteForm
+        setShowModal={setShowModal}
+        currentWebSite={getUser.webSite}
+        refetch={refetch}
+      />
+    );
+  };
+
   const onLogout = () => {
     client.clearStore();
     logout();
@@ -46,8 +70,8 @@ function SettingForm({
         Cambiar contraseña
       </Button>
       <Button onClick={onChangeEmail}>Cambiar email</Button>
-      <Button>Descripción</Button>
-      <Button>Sitio web</Button>
+      <Button onClick={onChangeDescription}>Descripción</Button>
+      <Button onClick={onChangeWebSite}>Sitio web</Button>
       <Button onClick={onLogout}>Cerrar sección</Button>
       <Button onClick={() => setShowModal(false)}>
         Cancelar
